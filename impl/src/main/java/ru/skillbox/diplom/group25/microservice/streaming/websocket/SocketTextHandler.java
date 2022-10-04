2089bdd0e2c5c76ab53d99086a74a221c25e12c7
alex90bar <aleksandr.baranov@skillbox.ru>
2022-10-04 17:39:23 +0300

    Поменял FROM в Dockerfile, добавил зависимость для websocket

---
+++ impl/src/main/java/ru/skillbox/diplom/group25/microservice/streaming/websocket/SocketTextHandler.java
@@ -0,0 +1,15 @@
+package ru.skillbox.diplom.group25.microservice.streaming.websocket;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+/**
+ * SocketTextHandler
+ *
+ * @author alex90bar
+ */
+
+public class SocketTextHandler extends TextWebSocketHandler {
+
+}
+
+
